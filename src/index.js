bb350a4582fbaddfeaf1fb40b77639170570d1d8
HarshGupta123454 <harsh840gupta@gmail.com>
2022-12-11 14:40:45 +0530

    index js change

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
-import {ModalProvider} from "react-hooks-use-modal"
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <BrowserRouter>
-    <ModalProvider>
+    
     <App />
-    </ModalProvider>
+    
     </BrowserRouter>
   // </React.StrictMode>
 );
